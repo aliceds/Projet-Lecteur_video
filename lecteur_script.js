af137b354ab3c91a6758fa82d9fcb1a09d017857
aliceds <alice.desforges@live.fr>
2017-11-01 13:39:23 +0100

    Amélioration #1
    création de la liste de lecture

--- lecteur_script.js
+++ lecteur_script.js
@@ -1,4 +1,6 @@
 function main() {
+    var value = 0;
+
     var btnVolumeslider =document.getElementById("volumeslider");
     var btnPlay = document.getElementById("play-pause");
     var btnRew = document.getElementById("rew");
@@ -13,6 +15,9 @@ function main() {
     btnFastFwd.addEventListener("click", fastFwd);
     btnRestart.addEventListener("click", restart);
     btnMute.addEventListener("click", mute);
+
+    document.getElementById("elementListeLecture").setAttribute("style","display:none");
+    document.getElementById("ajouterLien").addEventListener("click", cloner);
 
     function playVideo() {
        if (video.paused) {
@@ -46,6 +51,42 @@ function main() {
         } else {
             video.muted = true;
         }
+    }
+
+    function cloner() {
+        var itm = document.getElementById("elementListeLecture");
+        var cln = itm.cloneNode(true);
+        value++;
+        cln.setAttribute("value", value);
+        cln.setAttribute("style","display: auto");
+        var lien = document.getElementById("lien").value;
+        document.getElementById("liste").appendChild(cln);
+        cln.getElementsByClassName("lienPlaylist")[0].setAttribute("value", lien);
+        cln.getElementsByClassName("supprimerLien")[0].addEventListener("click", function() {
+            cln.parentNode.removeChild(cln);
+        });
+
+        cln.getElementsByClassName("monterLien")[0].addEventListener("click", function() {
+            if (cln !== cln.parentNode.firstChild) {
+                var elementParent = cln.parentNode;
+                var tmp = cln;
+                var precedent = cln.previousSibling;
+                elementParent.removeChild(cln);
+                elementParent.insertBefore(tmp, precedent);
+            }
+        });
+
+        cln.getElementsByClassName("descendreLien")[0].addEventListener("click", function() {
+            if (cln !== cln.parentNode.lastChild) {
+                var elementParent = cln.parentNode;
+                var tmp = cln;
+                var suivant = cln.nextSibling;
+                elementParent.removeChild(cln);
+                elementParent.insertBefore(tmp, suivant.nextSibling);
+            }
+        });
+
+        console.log(lien);
     }
 }
 
